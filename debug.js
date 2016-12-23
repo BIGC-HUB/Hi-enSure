31b5d184678855ccd5e789ff5f0079a7ff319802
大海 <iwangyang@qq.com>
2016-12-23 15:02:48 +0800

    载入 data model 开始钻研！

--- debug.js
+++ debug.js
@@ -6,56 +6,286 @@ var log = function() {
 // var template = window['tempHtml']["person"],
 // html = template({"firstname": "熊", "lastname": "猫"});
 // $('#person').html(html)
-
+var data = {
+  "data": {
+    "data": [
+      {
+        "prop_memo": "",
+        "account_crt_id_name": "gaoqr",
+        "code": "Z001",
+        "memo": "Z001",
+        "rid": 1,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z001",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 50.00,
+        "account_crt_id": "ruyizang:15201218752",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠216天然合成清宫红玛瑙5.5mm正圆收藏级红T1203天1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠216天然合成清宫红玛瑙5.5mm正圆收藏级红T1203天1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z200",
+        "memo": "",
+        "rid": 2,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z200",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 60.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠手串纯天然汉传白水晶14mm正圆精选级透明TJ14颗1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠手串纯天然汉传白水晶14mm正圆精选级透明TJ14颗1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z201",
+        "memo": "",
+        "rid": 3,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z201",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 65.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠手串纯天然汉传白水晶14mm正圆精选级透明TJ14颗1203Z201满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠手串纯天然汉传白水晶14mm正圆精选级透明TJ14颗1203Z201满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z002",
+        "memo": "",
+        "rid": 4,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z002",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 42.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠108天然合成藏式红玛瑙8mm椭圆精选级红T1203满2",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠108天然合成藏式红玛瑙8mm椭圆精选级红T1203满2",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z202",
+        "memo": "",
+        "rid": 5,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z202",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 51.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠手串纯天然汉传白水晶13mm正圆精选级透明TJ14颗1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠手串纯天然汉传白水晶13mm正圆精选级透明TJ14颗1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z100",
+        "memo": "",
+        "rid": 6,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z100",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 60.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠手串天然合成汉传红玛瑙14mm正圆精选级红TJ14颗1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠手串天然合成汉传红玛瑙14mm正圆精选级红TJ14颗1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z003",
+        "memo": "",
+        "rid": 7,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z003",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 27.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠108天然合成汉传红玛瑙6.5mm正圆精选级红T1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠108天然合成汉传红玛瑙6.5mm正圆精选级红T1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z004",
+        "memo": "",
+        "rid": 8,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z004",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 14.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠手串天然合成汉传黑玛瑙8mm正圆精选级黑TJ20颗1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠手串天然合成汉传黑玛瑙8mm正圆精选级黑TJ20颗1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z005",
+        "memo": "",
+        "rid": 9,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z005",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 57.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠108天然合成汉传黑玛瑙8mm正圆精选级黑T1203满1",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠108天然合成汉传黑玛瑙8mm正圆精选级黑T1203满1",
+        "category": "",
+        "status": 5
+      },
+      {
+        "prop_memo": "",
+        "code": "Z006",
+        "memo": "",
+        "rid": 10,
+        "price": 0.00,
+        "lm_ts": 1470157994000,
+        "id": "ruyizang:Z006",
+        "tenantry_id": "ruyizang",
+        "brand": "",
+        "weight": 28.00,
+        "account_crt_id": "",
+        "cate_id": "",
+        "avatar": "",
+        "volume": 0.00,
+        "is_combo": 0,
+        "is_gift": 0,
+        "sales_name": "佛珠108天然合成汉传红玛瑙6mm正圆精选级红T1203满2",
+        "crt_ts": 1470157994000,
+        "has_sku": 3,
+        "bar_code": "",
+        "name": "佛珠108天然合成汉传红玛瑙6mm正圆精选级红T1203满2",
+        "category": "",
+        "status": 5
+      }
+    ],
+    "pageNo": 1,
+    "pageSize": 10,
+    "totalCount": 5495,
+    "totalPageCount": 550
+  },
+  "status": 1
+}
+var model = {
+    "code": "",
+    "name": '柱状图',
+    "memo": "",
+    "ds_code": "bind_dataset_code",//所属数据集
+    "show_type": "bar",//显示类型
+    "fields": [{
+        "series": "x", //所在轴
+        "code": "name",//字段编码
+        "type": "code",//数据类型
+    },{
+        "series": "y",   //所在轴
+        "code": "weight",//字段编码
+        "type": "code",  //数据类型
+    }]
+}
 
 // 气泡图 bubble
-var bubbleData = [{
-    'class': '2048',
-    'shuju': [
-        [44056, 81.8, 23968973, 'Australia', 2015],
-        [43294, 81.7, 35939927, 'Canada', 2015],
-        [13334, 76.9, 1376048943, 'China', 2015],
-        [21291, 78.5, 11389562, 'Cuba', 2015],
-        [38923, 80.8, 5503457, 'Finland', 2015],
-        [37599, 81.9, 64395345, 'France', 2015],
-        [44053, 81.1, 80688545, 'Germany', 2015],
-        [42182, 82.8, 329425, 'Iceland', 2015],
-        [5903, 66.8, 1311050527, 'India', 2015],
-        [36162, 83.5, 126573481, 'Japan', 2015],
-        [1390, 71.4, 25155317, 'North Korea', 2015],
-        [34644, 80.7, 50293439, 'South Korea', 2015],
-        [34186, 80.6, 4528526, 'New Zealand', 2015],
-        [64304, 81.6, 5210967, 'Norway', 2015],
-        [24787, 77.3, 38611794, 'Poland', 2015],
-        [23038, 73.13, 143456918, 'Russia', 2015],
-        [19360, 76.5, 78665830, 'Turkey', 2015],
-        [38225, 81.4, 64715810, 'United Kingdom', 2015],
-        [53354, 79.1, 321773631, 'United States', 2015]
-    ],
-}, {
-    'class': '2012',
-    'shuju': [
-        [28604, 77, 17096869, 'Australia', 1990],
-        [31163, 77.4, 27662440, 'Canada', 1990],
-        [1516, 68, 1154605773, 'China', 1990],
-        [13670, 74.7, 10582082, 'Cuba', 1990],
-        [28599, 75, 4986705, 'Finland', 1990],
-        [29476, 77.1, 56943299, 'France', 1990],
-        [31476, 75.4, 78958237, 'Germany', 1990],
-        [28666, 78.1, 254830, 'Iceland', 1990],
-        [1777, 57.7, 870601776, 'India', 1990],
-        [29550, 79.1, 122249285, 'Japan', 1990],
-        [2076, 67.9, 20194354, 'North Korea', 1990],
-        [12087, 72, 42972254, 'South Korea', 1990],
-        [24021, 75.4, 3397534, 'New Zealand', 1990],
-        [43296, 76.8, 4240375, 'Norway', 1990],
-        [10088, 70.8, 38195258, 'Poland', 1990],
-        [19349, 69.6, 147568552, 'Russia', 1990],
-        [10670, 67.3, 53994605, 'Turkey', 1990],
-        [26424, 75.7, 57110117, 'United Kingdom', 1990],
-        [37062, 75.4, 252847810, 'United States', 1990]
-    ],
-}]
 var __initBubble__ = function(title, Data, Dom) {
     var list = []
     var className = []
@@ -112,15 +342,6 @@ var __initBubble__ = function(title, Data, Dom) {
 }
 
 // 折线图 line - xAxis
-var lineData = [{
-    name: '邮件营销',
-    type: 'line',
-    data: [120, 132, 101, 134, 90, 230, 210]
-}, {
-    name: '搜索引擎',
-    type: 'line',
-    data: [820, 932, 901, 934, 1290, 1330, 1320]
-}]
 var __initLine__ = function(title, Data, Dom) {
     var xAxis = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
     var className = []
@@ -162,12 +383,6 @@ var __initLine__ = function(title, Data, Dom) {
 }
 
 // 柱形图 bar
-var barData = [{
-    name: '直接访问',
-    type: 'bar',
-    barWidth: '60%', //不必须
-    data: [10, 52, 200, 334, 390, 330, 220]
-}]
 var __initBar__ = function(title, Data, Dom) {
     var xAxis = ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
     var className = []
@@ -210,22 +425,6 @@ var __initBar__ = function(title, Data, Dom) {
 }
 
 // 饼图 pie
-var pieData = [{
-        value: 335,
-        name: '直接访问'
-    }, {
-        value: 310,
-        name: '邮件营销'
-    }, {
-        value: 234,
-        name: '联盟广告'
-    }, {
-        value: 135,
-        name: '视频广告'
-    }, {
-        value: 1548,
-        name: '搜索引擎'
-    }]
 var __initPie__ = function(title, Data, Dom) {
     option = {
         title: {
@@ -271,16 +470,9 @@ var __initPie__ = function(title, Data, Dom) {
 }
 
 // 雷达图 radar
-var radarData = [{
-    value: [4300, 10000, 28000, 35000, 50000, 19000],
-    name: '预算分配（Allocated Budget）'
-    }, {
-    value: [5000, 14000, 28000, 31000, 42000, 21000],
-    name: '实际开销（Actual Spending）'
-    }]
 var __initRadar__ = function(title, Data, Dom) {
     var className = []
-    for (let i of radarData) {
+    for (let i of Data) {
         className.push(i.name)
     }
     var indicator = [{
@@ -318,92 +510,190 @@ var __initRadar__ = function(title, Data, Dom) {
             // name: '预算 vs 开销（Budget vs spending）',
             type: 'radar',
             // areaStyle: {normal: {}}, //填充色
-            data: radarData
+            data: Data
         }]
     };
     echarts.init(Dom).setOption(option)
 }
 
 // funnel 漏斗图
-var option = {
-    title: {
-        text: '漏斗图',
-        subtext: '纯属虚构'
-    },
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b} : {c}%"
-    },
-    toolbox: {
-        feature: {
-            dataView: {readOnly: false},
-            restore: {},
-            saveAsImage: {}
-        }
-    },
-    legend: {
-        data: ['展现','点击','访问','咨询','订单']
-    },
-    calculable: true,
-    series: [
-        {
-            name:'漏斗图',
-            type:'funnel',
-            left: '10%',
-            top: 60,
-            //x2: 80,
-            bottom: 60,
-            width: '80%',
-            // height: {totalHeight} - y - y2,
-            min: 0,
-            max: 100,
-            minSize: '0%',
-            maxSize: '100%',
-            sort: 'descending',
-            gap: 2,
-            label: {
-                normal: {
-                    show: true,
-                    position: 'inside'
+var __initFunnel__ = function(title, Data, Dom) {
+    var className = []
+    for (var i of Data) {
+        className.push(i.name)
+    }
+    var option = {
+        title: {
+            text: title,
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c}%"
+        },
+        toolbox: {
+            feature: {
+                dataView: {readOnly: false},
+                restore: {},
+                saveAsImage: {}
+            }
+        },
+        legend: {
+            data: className
+        },
+        calculable: true,
+        series: [
+            {
+                name:'漏斗图',
+                type:'funnel',
+                left: '10%',
+                top: 60,
+                //x2: 80,
+                bottom: 60,
+                width: '80%',
+                // height: {totalHeight} - y - y2,
+                min: 0,
+                max: 100,
+                minSize: '0%',
+                maxSize: '100%',
+                sort: 'descending',
+                gap: 2,
+                label: {
+                    normal: {
+                        show: true,
+                        position: 'inside'
+                    },
+                    emphasis: {
+                        textStyle: {
+                            fontSize: 20
+                        }
+                    }
                 },
-                emphasis: {
-                    textStyle: {
-                        fontSize: 20
+                labelLine: {
+                    normal: {
+                        length: 10,
+                        lineStyle: {
+                            width: 1,
+                            type: 'solid'
+                        }
                     }
-                }
-            },
-            labelLine: {
-                normal: {
-                    length: 10,
-                    lineStyle: {
-                        width: 1,
-                        type: 'solid'
+                },
+                itemStyle: {
+                    normal: {
+                        borderColor: '#fff',
+                        borderWidth: 1
                     }
-                }
-            },
-            itemStyle: {
-                normal: {
-                    borderColor: '#fff',
-                    borderWidth: 1
-                }
-            },
-            data: [
-                {value: 60, name: '访问'},
-                {value: 40, name: '咨询'},
-                {value: 20, name: '订单'},
-                {value: 80, name: '点击'},
-                {value: 100, name: '展现'}
-            ]
-        }
-    ]
-};
-echarts.init($('#id-funnel')[0]).setOption(option)
+                },
+                data: Data
+            }
+        ]
+    }
+    echarts.init(Dom).setOption(option)
+}
 
 var __init__ = function() {
-        __initBar__('Echo柱形图', barData, $('#id-bar')[0])
-        __initBubble__('疾风之刃气泡图', bubbleData, $('#id-bubble')[0])
-        __initPie__('饼图', pieData, $('#id-pie')[0])
-        __initLine__('折线图堆叠', lineData, $('#id-line')[0])
-        __initRadar__('阿姆斯特朗回旋踢', radarData, $('#id-radar')[0])
-    }
+    // 柱形图DATA
+    var barData = [{
+        name: '直接访问',
+        type: 'bar',
+        barWidth: '60%', //不必须
+        data: [10, 52, 200, 334, 390, 330, 220]
+    }]
+    __initBar__('Echo柱形图', barData, $('#id-bar')[0])
+    // 气泡图DATA
+    var bubbleData = [{
+        'class': '2048',
+        'shuju': [
+            [44056, 81.8, 23968973, 'Australia', 2015],
+            [43294, 81.7, 35939927, 'Canada', 2015],
+            [13334, 76.9, 1376048943, 'China', 2015],
+            [21291, 78.5, 11389562, 'Cuba', 2015],
+            [38923, 80.8, 5503457, 'Finland', 2015],
+            [37599, 81.9, 64395345, 'France', 2015],
+            [44053, 81.1, 80688545, 'Germany', 2015],
+            [42182, 82.8, 329425, 'Iceland', 2015],
+            [5903, 66.8, 1311050527, 'India', 2015],
+            [36162, 83.5, 126573481, 'Japan', 2015],
+            [1390, 71.4, 25155317, 'North Korea', 2015],
+            [34644, 80.7, 50293439, 'South Korea', 2015],
+            [34186, 80.6, 4528526, 'New Zealand', 2015],
+            [64304, 81.6, 5210967, 'Norway', 2015],
+            [24787, 77.3, 38611794, 'Poland', 2015],
+            [23038, 73.13, 143456918, 'Russia', 2015],
+            [19360, 76.5, 78665830, 'Turkey', 2015],
+            [38225, 81.4, 64715810, 'United Kingdom', 2015],
+            [53354, 79.1, 321773631, 'United States', 2015]
+        ],
+    }, {
+        'class': '2012',
+        'shuju': [
+            [28604, 77, 17096869, 'Australia', 1990],
+            [31163, 77.4, 27662440, 'Canada', 1990],
+            [1516, 68, 1154605773, 'China', 1990],
+            [13670, 74.7, 10582082, 'Cuba', 1990],
+            [28599, 75, 4986705, 'Finland', 1990],
+            [29476, 77.1, 56943299, 'France', 1990],
+            [31476, 75.4, 78958237, 'Germany', 1990],
+            [28666, 78.1, 254830, 'Iceland', 1990],
+            [1777, 57.7, 870601776, 'India', 1990],
+            [29550, 79.1, 122249285, 'Japan', 1990],
+            [2076, 67.9, 20194354, 'North Korea', 1990],
+            [12087, 72, 42972254, 'South Korea', 1990],
+            [24021, 75.4, 3397534, 'New Zealand', 1990],
+            [43296, 76.8, 4240375, 'Norway', 1990],
+            [10088, 70.8, 38195258, 'Poland', 1990],
+            [19349, 69.6, 147568552, 'Russia', 1990],
+            [10670, 67.3, 53994605, 'Turkey', 1990],
+            [26424, 75.7, 57110117, 'United Kingdom', 1990],
+            [37062, 75.4, 252847810, 'United States', 1990]
+        ],
+    }]
+    __initBubble__('疾风之刃气泡图', bubbleData, $('#id-bubble')[0])
+    // 饼图DATA
+    var pieData = [{
+            value: 335,
+            name: '直接访问'
+    }, {
+            value: 310,
+            name: '邮件营销'
+        }, {
+            value: 234,
+            name: '联盟广告'
+        }, {
+            value: 135,
+            name: '视频广告'
+        }, {
+            value: 1548,
+            name: '搜索引擎'
+        }]
+    __initPie__('饼图', pieData, $('#id-pie')[0])
+    // 折线图DATA
+    var lineData = [{
+            name: '邮件营销',
+            type: 'line',
+            data: [120, 132, 101, 134, 90, 230, 210]
+    }, {
+            name: '搜索引擎',
+            type: 'line',
+            data: [820, 932, 901, 934, 1290, 1330, 1320]
+        }]
+    __initLine__('折线图堆叠', lineData, $('#id-line')[0])
+    // 雷达图DATA
+    var radarData = [{
+        value: [4300, 10000, 28000, 35000, 50000, 19000],
+        name: '预算分配（Allocated Budget）'
+        }, {
+        value: [5000, 14000, 28000, 31000, 42000, 21000],
+        name: '实际开销（Actual Spending）'
+        }]
+    __initRadar__('雷达图', radarData, $('#id-radar')[0])
+    // 漏斗图DATA
+    var funnelData = [
+        {value: 60, name: '亚索'},
+        {value: 40, name: '瞎子'},
+        {value: 20, name: '瑞文'},
+        {value: 80, name: '梦魇'},
+        {value: 100, name: '石头人'},
+    ]
+    __initFunnel__('漏斗图', funnelData, $('#id-funnel')[0])
+}
 __init__()
