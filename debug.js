cbf2ab8690c03e35aaf6caaf0fc6f1f7929af8d9
大海 <iwangyang@qq.com>
2016-12-27 11:11:07 +0800

    漏斗图 气泡图 收工！

--- debug.js
+++ debug.js
@@ -257,6 +257,7 @@ var data = {
     },
     "status": 1
 }
+
 var barModel = {
     "code": "柱状图",
     "name": '高度',
@@ -316,6 +317,31 @@ var pieModel = {
         "name": "name",
         }]
 }
+var funnelModel = {
+    "code": "漏斗图",
+    "name": "高度",
+    "memo": "",
+    "ds_code": "bind_dataset_code",
+    "show_type": "funnel",//显示类型
+    "fields": [{
+        "value": "weight",
+        "name": "name",
+        }]
+}
+var bubbleModel = {
+    "code": "气泡图",
+    "name": "质量",
+    "memo": "",
+    "ds_code": "bind_dataset_code",
+    "show_type": "bubble",//显示类型
+    "fields": [{
+        "xAxis": "weight",
+        "yAxis": "weight",
+        "size": "weight",
+        "name": "name",
+        "class": "tenantry_id"
+        }]
+}
 
 var Data = function(data, model) {
     var result = []
@@ -361,8 +387,8 @@ var Data = function(data, model) {
                 }
             }
         }
-    } else if (model.show_type === "pie") {
-        var pieData = []
+    } else if (model.show_type === "pie" || model.show_type === "funnel") {
+        var tempData = []
         for (let _data of data.data.data) {
             var tempObj = {}
             for (let field of model.fields) {
@@ -370,15 +396,26 @@ var Data = function(data, model) {
                     tempObj[key] = _data[field[key]]
                 }
             }
-            pieData.push(tempObj)
+            tempData.push(tempObj)
         }
-        j.data = pieData
+        j.data = tempData
+    } else if (model.show_type === "bubble") {
+        var finalList = []
+        for (let i of data.data.data) {
+            var tempList = []
+            for (let field of model.fields) {
+                for (let key in field) {
+                    tempList.push(i[field[key]])
+                }
+            }
+            finalList.push(tempList)
+        }
+        j.class = tempList[4]
+        j.shuju = finalList
     }
-
     result.push(j)
     return result
 }
-
 // 柱形图 bar
 var __initBar__ = function(Data, Dom) {
     var xAxis = Data[0].xAxis
@@ -512,28 +549,24 @@ var __initPie__ = function(Data, Dom) {
             label: {
                 normal: {
                     textStyle: {
-                        fontSize: 18,
-                        color: '#235894'
+                        fontSize: 16,
+                        color: '#333'
                     }
                 }
             },
             labelLine: {
                 normal: {
                     lineStyle: {
-                        color: '#235894'
+                        color: '#333'
                     }
                 }
             },
             data: Data,
             itemStyle: {
                 normal: {
-                    opacity: 0.7,
-                    color: {
-                        image: piePatternImg, // img.js文件
-                        repeat: 'repeat'
-                    },
-                    borderWidth: 3,
-                    borderColor: '#235894'
+                    // opacity: 0.7, //透明度
+                    borderWidth: 2,
+                    borderColor: '#333'
                 }
             }
         }]
@@ -542,17 +575,19 @@ var __initPie__ = function(Data, Dom) {
 }
 
 // 气泡图 bubble
-var __initBubble__ = function(title, Data, Dom) {
+var __initBubble__ = function(Data, Dom) {
+    log(Data[0])
+    var title = Data[0].title
     var list = []
     var className = []
-    var divide = 500 //气泡大小倍数, 若无倍数 则除以 1
+    var divide = 10 //气泡大小倍数, 若无倍数 则除以 1
     for (let i of Data) {
         let e = {
             name: i.class,
             data: i.shuju,
             type: 'scatter',
             symbolSize: function(data) {
-                return Math.sqrt(data[2]) / divide;
+                return Math.sqrt(data[2]) * divide;
             },
             label: {
                 emphasis: {
@@ -598,7 +633,9 @@ var __initBubble__ = function(title, Data, Dom) {
 }
 
 // funnel 漏斗图
-var __initFunnel__ = function(title, Data, Dom) {
+var __initFunnel__ = function(Data, Dom) {
+    var title = Data[0].title
+    var Data = Data[0].data
     var className = []
     for (var i of Data) {
         className.push(i.name)
@@ -676,55 +713,57 @@ var __init__ = function() {
     __initLine__( Data(data, lineModel),  $('#id-line')[0])
     __initRadar__(Data(data, radarModel), $('#id-radar')[0])
     __initPie__(  Data(data, pieModel),   $('#id-pie')[0])
+    __initFunnel__(Data(data, funnelModel), $('#id-funnel')[0])
+    __initBubble__(Data(data, bubbleModel), $('#id-bubble')[0])
     // 气泡图DATA (Model自己编)
     var bubbleData = [{
         'class': '2048',
         'shuju': [
-            [44056, 81.8, 23968973, 'Australia', 2015],
-            [43294, 81.7, 35939927, 'Canada', 2015],
+            // [44056, 81.8, 23968973, 'Australia', 2015],
+            // [43294, 81.7, 35939927, 'Canada', 2015],
             [13334, 76.9, 1376048943, 'China', 2015],
-            [21291, 78.5, 11389562, 'Cuba', 2015],
-            [38923, 80.8, 5503457, 'Finland', 2015],
-            [37599, 81.9, 64395345, 'France', 2015],
-            [44053, 81.1, 80688545, 'Germany', 2015],
-            [42182, 82.8, 329425, 'Iceland', 2015],
-            [5903, 66.8, 1311050527, 'India', 2015],
-            [36162, 83.5, 126573481, 'Japan', 2015],
-            [1390, 71.4, 25155317, 'North Korea', 2015],
-            [34644, 80.7, 50293439, 'South Korea', 2015],
-            [34186, 80.6, 4528526, 'New Zealand', 2015],
-            [64304, 81.6, 5210967, 'Norway', 2015],
-            [24787, 77.3, 38611794, 'Poland', 2015],
-            [23038, 73.13, 143456918, 'Russia', 2015],
-            [19360, 76.5, 78665830, 'Turkey', 2015],
-            [38225, 81.4, 64715810, 'United Kingdom', 2015],
-            [53354, 79.1, 321773631, 'United States', 2015]
-        ],
-    }, {
-        'class': '2012',
-        'shuju': [
-            [28604, 77, 17096869, 'Australia', 1990],
-            [31163, 77.4, 27662440, 'Canada', 1990],
-            [1516, 68, 1154605773, 'China', 1990],
-            [13670, 74.7, 10582082, 'Cuba', 1990],
-            [28599, 75, 4986705, 'Finland', 1990],
-            [29476, 77.1, 56943299, 'France', 1990],
-            [31476, 75.4, 78958237, 'Germany', 1990],
-            [28666, 78.1, 254830, 'Iceland', 1990],
-            [1777, 57.7, 870601776, 'India', 1990],
-            [29550, 79.1, 122249285, 'Japan', 1990],
-            [2076, 67.9, 20194354, 'North Korea', 1990],
-            [12087, 72, 42972254, 'South Korea', 1990],
-            [24021, 75.4, 3397534, 'New Zealand', 1990],
-            [43296, 76.8, 4240375, 'Norway', 1990],
-            [10088, 70.8, 38195258, 'Poland', 1990],
-            [19349, 69.6, 147568552, 'Russia', 1990],
-            [10670, 67.3, 53994605, 'Turkey', 1990],
-            [26424, 75.7, 57110117, 'United Kingdom', 1990],
-            [37062, 75.4, 252847810, 'United States', 1990]
-        ],
+            // [21291, 78.5, 11389562, 'Cuba', 2015],
+            // [38923, 80.8, 5503457, 'Finland', 2015],
+            // [37599, 81.9, 64395345, 'France', 2015],
+            // [44053, 81.1, 80688545, 'Germany', 2015],
+            // [42182, 82.8, 329425, 'Iceland', 2015],
+            // [5903, 66.8, 1311050527, 'India', 2015],
+            // [36162, 83.5, 126573481, 'Japan', 2015],
+            // [1390, 71.4, 25155317, 'North Korea', 2015],
+            // [34644, 80.7, 50293439, 'South Korea', 2015],
+            // [34186, 80.6, 4528526, 'New Zealand', 2015],
+            // [64304, 81.6, 5210967, 'Norway', 2015],
+            // [24787, 77.3, 38611794, 'Poland', 2015],
+            // [23038, 73.13, 143456918, 'Russia', 2015],
+            // [19360, 76.5, 78665830, 'Turkey', 2015],
+            // [38225, 81.4, 64715810, 'United Kingdom', 2015],
+            // [53354, 79.1, 321773631, 'United States', 2015]
+        ]
     }]
-    __initBubble__('疾风之刃气泡图', bubbleData, $('#id-bubble')[0])
+    // , {
+    //     'class': '2012',
+    //     'shuju': [
+    //         [28604, 77, 17096869, 'Australia', 1990],
+    //         [31163, 77.4, 27662440, 'Canada', 1990],
+    //         [1516, 68, 1154605773, 'China', 1990],
+    //         [13670, 74.7, 10582082, 'Cuba', 1990],
+    //         [28599, 75, 4986705, 'Finland', 1990],
+    //         [29476, 77.1, 56943299, 'France', 1990],
+    //         [31476, 75.4, 78958237, 'Germany', 1990],
+    //         [28666, 78.1, 254830, 'Iceland', 1990],
+    //         [1777, 57.7, 870601776, 'India', 1990],
+    //         [29550, 79.1, 122249285, 'Japan', 1990],
+    //         [2076, 67.9, 20194354, 'North Korea', 1990],
+    //         [12087, 72, 42972254, 'South Korea', 1990],
+    //         [24021, 75.4, 3397534, 'New Zealand', 1990],
+    //         [43296, 76.8, 4240375, 'Norway', 1990],
+    //         [10088, 70.8, 38195258, 'Poland', 1990],
+    //         [19349, 69.6, 147568552, 'Russia', 1990],
+    //         [10670, 67.3, 53994605, 'Turkey', 1990],
+    //         [26424, 75.7, 57110117, 'United Kingdom', 1990],
+    //         [37062, 75.4, 252847810, 'United States', 1990]
+    //     ],
+    // }]
     // 饼图DATA
     var pieData = [{
             value: 335,
@@ -768,6 +807,5 @@ var __init__ = function() {
         {value: 80, name: '梦魇'},
         {value: 100, name: '石头人'},
     ]
-    __initFunnel__('漏斗图', funnelData, $('#id-funnel')[0])
 }
 __init__()
